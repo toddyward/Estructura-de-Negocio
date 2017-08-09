ebea18636aff793f918523e394b9e5a56434d49a
tkhacker <tkhacker@debian>
2017-08-09 02:22:01 -0400

    Lanzador de Reportes

---
+++ ProyectoProgramacion/src/otrasClases/LanzadorReportes.java
@@ -0,0 +1,135 @@
+package otrasClases;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.sql.Connection;
+import java.util.HashMap;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
+import net.sf.jasperreports.view.JRViewer;
+
+public class LanzadorReportes extends JDialog{
+
+	private static final long serialVersionUID = 308251665095677784L;
+	public HashMap<String, Object> parametros;
+	public String nombreArchivo; 
+
+	JasperPrint archivoImprimir;
+	JRViewer panelVisor;
+	
+	
+	public LanzadorReportes(final Frame padre, final String titulo){
+		
+		super(padre, titulo,true);
+		this.setResizable(true);
+		
+	}
+	public LanzadorReportes(){
+		
+		super();
+		
+	}
+	
+	/*public void cargarReporte(final String nombreArchivo,final HashMap<String, String> parametros, final JRResultSetDataSource datos){				
+		
+		try {
+			
+			this.archivoImprimir = JasperFillManager.fillReport(nombreArchivo, parametros,datos );
+			this.panelVisor= new JRViewer(this.archivoImprimir);
+			this.getContentPane().add(this.panelVisor,BorderLayout.CENTER);
+			
+		}catch(final JRException e) {		
+			
+			e.printStackTrace();
+			
+		}
+		
+	}*/
+	
+	/*public void cargarReporte(final String nombreArchivo,final HashMap<String, String> parametros, final JREmptyDataSource datos){
+	
+		
+	
+	}*/
+	
+
+/*	public void cargarReporte(final String nombreArchivo,final HashMap<String, String> parametros, final JRTableModelDataSource  datos){
+		
+		try {
+			
+			this.archivoImprimir = JasperFillManager.fillReport(nombreArchivo, parametros,datos );
+			this.panelVisor= new JRViewer(this.archivoImprimir);
+			this.getContentPane().add(this.panelVisor,BorderLayout.CENTER);
+
+		}catch(final JRException e) {
+
+			e.printStackTrace();
+		}	
+		
+	}*/
+
+	public void cargarReporte(String nombreArchivo, HashMap<String, Object> parametros, Connection datos){
+
+		try {
+	        JasperReport jasperReport =  JasperCompileManager.compileReport(nombreArchivo);
+			System.out.println("LLege aqui");
+	        this.parametros = parametros;
+	        
+			this.archivoImprimir = JasperFillManager.fillReport(jasperReport, this.parametros, datos);
+			this.panelVisor= new JRViewer(this.archivoImprimir);
+			this.getContentPane().add(this.panelVisor,BorderLayout.CENTER);
+			
+		} catch (final JRException e) {		
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+
+
+	/*public void cargarReporte(final String nombreArchivo,final HashMap<String, String> parametros, final JRBeanCollectionDataSource datos){
+
+		try {
+			
+			this.archivoImprimir = JasperFillManager.fillReport(nombreArchivo, parametros,datos );
+			this.panelVisor= new JRViewer(this.archivoImprimir);
+			this.getContentPane().add(this.panelVisor,BorderLayout.CENTER);
+
+		} catch (final JRException e) {
+
+			e.printStackTrace();
+			
+		}
+		
+	}*/
+
+	public void setVisible(final boolean ver){
+
+		if(this.panelVisor!=null)
+			super.setVisible(ver);
+		
+		else{
+
+			JOptionPane.showMessageDialog(this, // padre
+					"Se debe llenar el reporte antes de llamar al visor", // mensajito
+					"Reporte Vacio", //titulo
+					JOptionPane.ERROR_MESSAGE);
+
+			this.dispose();
+			
+		}
+	}
+}
+
